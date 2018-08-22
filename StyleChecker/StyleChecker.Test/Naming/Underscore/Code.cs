56458c01a2e5bf7002dc5ccd62c7a169f97f8825
Tomohisa Tanaka <Tomohisa.Tanaka@gmail.com>
2018-08-22 18:55:59 +0900

    Fix the Underscore analyzer to count Local Function and Parameter.

--- StyleChecker/StyleChecker.Test/Naming/Underscore/Code.cs
+++ StyleChecker/StyleChecker.Test/Naming/Underscore/Code.cs
@@ -4,20 +4,28 @@ namespace Application
 
     public sealed class Code
     {
-        public void OK()
-        {
-        }
-
-        public void NG()
+        public void LocalVarDecl()
         {
             var _alpha = 1;
             int foo_bar = 2;
             int _foo_bar_baz_ = 3;
-            void outString(out string s);
+        }
+
+        public void Param(string[] _args)
+        {
+            void outString(out string _s)
             {
-                s = "hello world";
+                _s = "hello world";
             };
             outString(out string _hello);
+            Action<int> _action
+                = _n => Console.WriteLine($"{_n}");
+            Action<int, int> _action2
+                = (_n, _m) => Console.WriteLine($"{_n},{_m}");
+            void _localFunc(int _v)
+            {
+                Console.WriteLine($"{_v}");
+            }
         }
     }
 }
--- StyleChecker/StyleChecker/Naming/Underscore/Analyzer.cs
+++ StyleChecker/StyleChecker/Naming/Underscore/Analyzer.cs
@@ -52,6 +52,9 @@ namespace StyleChecker.Naming.Underscore
             var root = context.Tree.GetCompilationUnitRoot(
                 context.CancellationToken);
             var all = new List<SyntaxToken>();
+            /*
+                Local variable declarations:
+            */
             all.AddRange(root.DescendantNodes()
                 .Where(n => n.IsKind(SyntaxKind.LocalDeclarationStatement))
                 .SelectMany(n => n.ChildNodes())
@@ -59,8 +62,9 @@ namespace StyleChecker.Naming.Underscore
                 .SelectMany(n => n.ChildNodes())
                 .Where(n => n.IsKind(SyntaxKind.VariableDeclarator))
                 .SelectMany(n => n.ChildTokens())
-                .Where(t => t.ToString().IndexOf('_') != -1)
-                .ToArray());
+                .Where(t => t.IsKind(SyntaxKind.IdentifierToken)
+                    && t.ToString().IndexOf('_') != -1)
+                .AsEnumerable());
             /*
                 Count out-var (Out Variable Declarations):
                 https://github.com/dotnet/csharplang/blob/master/proposals/csharp-7.0/out-var.md
@@ -72,8 +76,27 @@ namespace StyleChecker.Naming.Underscore
                 .SelectMany(n => n.ChildNodes())
                 .Where(n => n.IsKind(SyntaxKind.SingleVariableDesignation))
                 .SelectMany(n => n.ChildTokens())
-                .Where(t => t.ToString().IndexOf('_') != -1)
-                .ToArray());
+                .Where(t => t.IsKind(SyntaxKind.IdentifierToken)
+                    && t.ToString().IndexOf('_') != -1)
+                .AsEnumerable());
+            /*
+                Parameters:
+            */
+            all.AddRange(root.DescendantNodes()
+                .Where(n => n.IsKind(SyntaxKind.Parameter))
+                .SelectMany(n => n.ChildTokens())
+                .Where(t => t.IsKind(SyntaxKind.IdentifierToken)
+                    && t.ToString().IndexOf('_') != -1)
+                .AsEnumerable());
+            /*
+                Local function:
+            */
+            all.AddRange(root.DescendantNodes()
+                .Where(n => n.IsKind(SyntaxKind.LocalFunctionStatement))
+                .SelectMany(n => n.ChildTokens())
+                .Where(t => t.IsKind(SyntaxKind.IdentifierToken)
+                    && t.ToString().IndexOf('_') != -1)
+                .AsEnumerable());
             if (all.Count == 0)
             {
                 return;
