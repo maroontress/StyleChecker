97e5793dd6181563a7d638af3dd4a1c60fedf5fa
Tomohisa Tanaka <Tomohisa.Tanaka@gmail.com>
2018-09-07 23:29:14 +0900

    Add InvalidConfig analyzer.

--- StyleChecker/StyleChecker/Categories.cs
+++ StyleChecker/StyleChecker/Categories.cs
@@ -24,5 +24,10 @@ namespace StyleChecker
         /// The category of naming rules.
         /// </summary>
         public const string Naming = nameof(Naming);
+
+        /// <summary>
+        /// The category of settings.
+        /// </summary>
+        public const string Settings = nameof(Settings);
     }
 }
