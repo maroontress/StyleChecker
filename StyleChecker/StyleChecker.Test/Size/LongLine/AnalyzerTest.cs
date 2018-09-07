1ce64b003e98ceaffbcdb82c631abbda0db051b9
Tomohisa Tanaka <Tomohisa.Tanaka@gmail.com>
2018-09-07 20:13:27 +0900

    Fix LongLine analyzer to count XML documentation comment.

--- StyleChecker/StyleChecker.Test/Size/LongLine/AnalyzerTest.cs
+++ StyleChecker/StyleChecker.Test/Size/LongLine/AnalyzerTest.cs
@@ -44,5 +44,48 @@ namespace StyleChecker.Test.Size.LongLine
                 Environment.Default,
                 expected(0, 82));
         }
+
+        [TestMethod]
+        public void DocumentComment()
+        {
+            var code = ReadText("DocumentComment");
+            var startOffset = 10;
+            DiagnosticResult expected(int row, int col)
+                => new DiagnosticResult
+            {
+                Id = Analyzer.DiagnosticId,
+                Message = string.Format(
+                    "The length of this line must be less than {0}.",
+                    "80"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations = SingleLocation(startOffset + row, col)
+            };
+            VerifyCSharpDiagnostic(
+                code,
+                Environment.Default,
+                expected(0, 87));
+        }
+
+        [TestMethod]
+        public void Config()
+        {
+            var code = ReadText("Code");
+            var startOffset = 3;
+            DiagnosticResult expected(int row, int col)
+                => new DiagnosticResult
+            {
+                Id = Analyzer.DiagnosticId,
+                Message = string.Format(
+                    "The length of this line must be less than {0}.",
+                    "20"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations = SingleLocation(startOffset + row, col)
+            };
+            var configText = $"<config maxLineLength=\"20\" />";
+            VerifyCSharpDiagnostic(
+                code,
+                Environment.Default.WithConfigText(configText),
+                expected(0, 22));
+        }
     }
 }
---
+++ StyleChecker/StyleChecker.Test/Size/LongLine/DocumentComment.cs
@@ -0,0 +1,19 @@
+#pragma warning disable CS0219
+
+namespace Application
+{
+    public sealed class Code
+    {
+//      8      16      24      32      40      48      56      64      72
+//      |       |       |       |       |       |       |       |       |
+        /// <summary>
+        ///                                                     This line is too long.
+        ///                                               Second long line is ignored.
+        /// </summary>
+        public void NG()
+        {
+// ignored other than the first long line.
+            int                                                         alpha = 1;
+        }
+    }
+}
