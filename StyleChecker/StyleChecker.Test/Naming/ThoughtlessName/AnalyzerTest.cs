d5339419fa650c83b7e2f649abe0a5e7d9dfeb20
Tomohisa Tanaka <Tomohisa.Tanaka@gmail.com>
2018-10-07 06:39:46 +0900

    Fix ThoughtlessName analyzer to report that Hungarian notation is used.

--- StyleChecker/StyleChecker.Test/Naming/ThoughtlessName/AnalyzerTest.cs
+++ StyleChecker/StyleChecker.Test/Naming/ThoughtlessName/AnalyzerTest.cs
@@ -28,26 +28,54 @@ namespace StyleChecker.Test.Naming.ThoughtlessName
         public void Code()
         {
             var code = ReadText("Code");
-            var startOffset = 10;
-            string Arconym(string token, string type)
-                => $"'{token}' is probably an acronym of its type name "
-                    + $"'{type}'";
             DiagnosticResult Expected(
-                int row, int col, string token, string type)
-                => new DiagnosticResult
+                int row,
+                int col,
+                string token,
+                System.Func<string, string, string> reason,
+                string type) => new DiagnosticResult
             {
                 Id = Analyzer.DiagnosticId,
                 Message = $"The name '{token}' is too easy: "
-                    + $"{Arconym(token, type)}",
+                    + $"{reason(token, type)}",
                 Severity = DiagnosticSeverity.Warning,
-                Locations = SingleLocation(startOffset + row, col),
+                Locations = SingleLocation(row, col),
             };
+
+            string Arconym(string token, string type)
+                => $"'{token}' is probably an acronym of its type name "
+                    + $"'{type}'";
+            DiagnosticResult ExpectedArconym(
+                int row, int col, string token, string type)
+                => Expected(row + 12, col, token, Arconym, type);
+
+            string HungarianPrefix(string token, string type)
+                => $"Hungarian notation is probably used for '{token}', "
+                    + $"because the type name is '{type}'";
+            DiagnosticResult ExpectedHungarianPrefix(
+                int row, int col, string token, string type)
+                => Expected(row + 18, col, token, HungarianPrefix, type);
 
             VerifyDiagnostic(
                 code,
                 Environment.Default,
-                Expected(0, 17, "sb", "StringBuilder"),
-                Expected(1, 17, "ms", "MemoryStream"));
+                ExpectedArconym(0, 17, "sb", "StringBuilder"),
+                ExpectedArconym(1, 17, "ms", "MemoryStream"),
+                ExpectedHungarianPrefix(0, 17, "bIsTrue", "bool"),
+                ExpectedHungarianPrefix(2, 17, "bRet", "byte"),
+                ExpectedHungarianPrefix(3, 17, "sbRet", "sbyte"),
+                ExpectedHungarianPrefix(5, 17, "iRet", "int"),
+                ExpectedHungarianPrefix(6, 17, "uiRet", "uint"),
+                ExpectedHungarianPrefix(8, 17, "sRet", "short"),
+                ExpectedHungarianPrefix(9, 17, "usRet", "ushort"),
+                ExpectedHungarianPrefix(11, 17, "lRet", "long"),
+                ExpectedHungarianPrefix(12, 17, "ulRet", "ulong"),
+                ExpectedHungarianPrefix(14, 17, "cRet", "char"),
+                ExpectedHungarianPrefix(16, 17, "fRet", "float"),
+                ExpectedHungarianPrefix(17, 17, "dRet", "double"),
+                ExpectedHungarianPrefix(19, 17, "dPrice", "decimal"),
+                ExpectedHungarianPrefix(20, 17, "sPrice", "string"),
+                ExpectedHungarianPrefix(21, 17, "oPrice", "object"));
         }
     }
 }
