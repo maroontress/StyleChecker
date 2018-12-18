7decd8f75b9be0d622b31274c6af0a1d4149890d
Tomohisa Tanaka <Tomohisa.Tanaka@gmail.com>
2018-12-19 00:28:55 +0900

    Add test cases of AssignmentToParameter analyzer.

--- StyleChecker/StyleChecker.Test/Refactoring/AssignmentToParameter/Okay.cs
+++ StyleChecker/StyleChecker.Test/Refactoring/AssignmentToParameter/Okay.cs
@@ -2,6 +2,20 @@ namespace StyleChecker.Test.Refactoring.AssignmentToParameter
 {
     public sealed class Okay
     {
+        private string message;
+
+        public string Property {
+            get
+            {
+                return message;
+            }
+
+            set
+            {
+                message = value;
+            }
+        }
+
         public void Ref(ref int value, out object o)
         {
             value = 0;
