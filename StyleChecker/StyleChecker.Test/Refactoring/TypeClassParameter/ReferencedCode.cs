fe3d967247b7195fd8d639a2b30454314d11e842
Tomohisa Tanaka (Maroontress) <Tomohisa.Tanaka@gmail.com>
2019-01-29 22:13:12 +0900

    Fix TypeClassParameter analyzer to count the documentation comments. (#80)

--- StyleChecker/StyleChecker.Test/Refactoring/TypeClassParameter/ReferencedCode.cs
+++ StyleChecker/StyleChecker.Test/Refactoring/TypeClassParameter/ReferencedCode.cs
@@ -6,15 +6,30 @@ namespace StyleChecker.Test.Refactoring.TypeClassParameter
 
     public sealed class ReferencedCode
     {
-        public static void Log(string message)
+        private static void Log(string message)
         {
         }
 
+        /// <summary>
+        /// Print the specified type.
+        /// </summary>
+        /// <param name="type">
+        /// The type to be printed.
+        /// </param>
         public static void PrintMethod(Type type)
         {
             Log(type.FullName);
         }
 
+        /// <summary>
+        /// Print the specified type.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type to be ignored.
+        /// </typeparam>
+        /// <param name="type">
+        /// The type to be printed.
+        /// </param>
         public void Print<T>(Type type)
         {
             Log(type.FullName);
