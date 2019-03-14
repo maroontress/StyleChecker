3b28bc7dab24b401a547f7e0acc8667d046dc242
Tomohisa Tanaka (Maroontress) <Tomohisa.Tanaka@gmail.com>
2019-03-15 08:08:41 +0900

    Add IsNull analyzer and CodeFix provider. (#97)

---
+++ StyleChecker/StyleChecker.Test/Refactoring/IsNull/AnalyzerTest.cs
@@ -0,0 +1,41 @@
+namespace StyleChecker.Test.Refactoring.IsNull
+{
+    using System.IO;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using StyleChecker.Refactoring.IsNull;
+    using StyleChecker.Test.Framework;
+
+    [TestClass]
+    public sealed class AnalyzerTest : CodeFixVerifier
+    {
+        public AnalyzerTest()
+            : base(
+                Path.Combine(Categories.Refactoring, "IsNull"),
+                new Analyzer(),
+                new CodeFixer())
+        {
+        }
+
+        [TestMethod]
+        public void Okay()
+            => VerifyDiagnostic(ReadText("Okay"), Atmosphere.Default);
+
+        [TestMethod]
+        public void Code()
+        {
+            var code = ReadText("Code");
+            var fix = ReadText("CodeFix");
+            Result Expected(Belief b)
+            {
+                var token = b.Message;
+                return b.ToResult(
+                    Analyzer.DiagnosticId,
+                    $"Use '{token}' operator instead of 'is' pattern matching.",
+                    DiagnosticSeverity.Info);
+            }
+
+            VerifyDiagnosticAndFix(code, Atmosphere.Default, Expected, fix);
+        }
+    }
+}
---
+++ doc/rules/IsNull.md
@@ -0,0 +1,62 @@
+# IsNull
+
+## Summary
+
+Use `== null` or `!= null` instead of `is null`.
+
+## Description
+
+This rule reports diagnostic information of using `is` pattern matching
+with the `null` constant literal.
+
+Note that the default diagnostic severity of this analyzer is
+[Information][diagnostic-severity].
+
+## Code fix
+
+The code fix provides an option replacing expression `... is null` and
+`!(... is null)` with `... == null` and `... != null`, respectively.
+
+### Remarks
+
+Replacing the expression `... is null` with `... == null`, as well as replacing
+`!(... is null)` with `... != null`, can be a breaking change.
+For more information, refer to
+[the description of EqualsNull code fix](../EqualsNull.md#Remarks).
+
+## Example
+
+### Diagnostic
+
+```csharp
+void Method(object o, string s)
+{
+    if (o is null)
+    {
+        ...
+    }
+    if (!(s is null))
+    {
+        ...
+    }
+    ...
+```
+
+### Code fix
+
+```csharp
+void Method(object o, string s)
+{
+    if (o == null)
+    {
+        ...
+    }
+    if (s != null)
+    {
+        ...
+    }
+    ...
+```
+
+[diagnostic-severity]:
+  https://docs.microsoft.com/en-us/dotnet/api/microsoft.codeanalysis.diagnosticseverity?view=roslyn-dotnet
