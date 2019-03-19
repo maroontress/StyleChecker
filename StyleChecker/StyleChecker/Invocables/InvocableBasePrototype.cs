bdcf94ef3508bb57b5b37bcc76a07cafb68b4ca7
Tomohisa Tanaka <Tomohisa.Tanaka@gmail.com>
2019-03-19 21:15:36 +0900

    Fix the API documents.
    - Fix CS1723 warnings.
    - Generate documentation comment XML files.
    - Remove BOM.

--- StyleChecker/StyleChecker/Invocables/InvocableBasePrototype.cs
+++ StyleChecker/StyleChecker/Invocables/InvocableBasePrototype.cs
@@ -14,50 +14,50 @@ namespace StyleChecker.Invocables
     public interface InvocableBasePrototype<out T>
     {
         /// <summary>
-        /// Gets a new <see cref="T"/> object with the specified <see
+        /// Gets a new <c>T</c> object with the specified <see
         /// cref="BlockSyntax"/> object.
         /// </summary>
         /// <param name="node">
         /// The <see cref="BlockSyntax"/> object.
         /// </param>
         /// <returns>
-        /// The new <see cref="T"/> object.
+        /// The new <c>T</c> object.
         /// </returns>
         T With(BlockSyntax node);
 
         /// <summary>
-        /// Gets a new <see cref="T"/> object with the specified <see
+        /// Gets a new <c>T</c> object with the specified <see
         /// cref="ParameterListSyntax"/> object.
         /// </summary>
         /// <param name="node">
         /// The <see cref="ParameterListSyntax"/> object.
         /// </param>
         /// <returns>
-        /// The new <see cref="T"/> object.
+        /// The new <c>T</c> object.
         /// </returns>
         T With(ParameterListSyntax node);
 
         /// <summary>
-        /// Gets a new <see cref="T"/> object with the specified <see
+        /// Gets a new <c>T</c> object with the specified <see
         /// cref="ArrowExpressionClauseSyntax"/> object.
         /// </summary>
         /// <param name="node">
         /// The <see cref="ArrowExpressionClauseSyntax"/> object.
         /// </param>
         /// <returns>
-        /// The new <see cref="T"/> object.
+        /// The new <c>T</c> object.
         /// </returns>
         T With(ArrowExpressionClauseSyntax node);
 
         /// <summary>
-        /// Gets a new <see cref="T"/> object with the specified <see
+        /// Gets a new <c>T</c> object with the specified <see
         /// cref="SyntaxToken"/> object representing a semicolon.
         /// </summary>
         /// <param name="node">
         /// The <see cref="SyntaxToken"/> object.
         /// </param>
         /// <returns>
-        /// The new <see cref="T"/> object.
+        /// The new <c>T</c> object.
         /// </returns>
         T With(SyntaxToken node);
     }
