79a7a9cec59d08727e285c98597e10ebf3bd6a88
Tomohisa Tanaka <Tomohisa.Tanaka@gmail.com>
2019-09-22 21:08:50 +0900

    Fix warnings.

--- StyleChecker/StyleChecker/Refactoring/StaticGenericClass/Analyzer.cs
+++ StyleChecker/StyleChecker/Refactoring/StaticGenericClass/Analyzer.cs
@@ -75,7 +75,7 @@ namespace StyleChecker.Refactoring.StaticGenericClass
 
                 bool IsClassTypeParameter(ISymbol s)
                     => s.Kind == SymbolKind.TypeParameter
-                        && s.ContainingSymbol == classSymbol;
+                        && Equals(s.ContainingSymbol, classSymbol);
                 bool IsTargetMethod(MethodDeclarationSyntax m)
                     => m.DescendantNodes()
                         .Where(n => n.IsKind(SyntaxKind.IdentifierName))
