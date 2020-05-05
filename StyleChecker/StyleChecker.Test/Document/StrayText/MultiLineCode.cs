ae76e03e92c72717d6b81a32e7b23a822cf8de1b
Tomohisa Tanaka <Tomohisa.Tanaka@gmail.com>
2020-05-06 02:12:09 +0900

    Add test cases.

--- StyleChecker/StyleChecker.Test/Document/StrayText/MultiLineCode.cs
+++ StyleChecker/StyleChecker.Test/Document/StrayText/MultiLineCode.cs
@@ -7,5 +7,38 @@ namespace StyleChecker.Test.Document.StrayText
         private void NotInTheTag()
         {
         }
+
+        /**
+         * Not in the tag.
+         */
+//@@      ^Not in the tag.
+        private void NotInTheTagWithLeadingAsterisks()
+        {
+        }
+
+        /**
+         * Before the tag.
+         * <summary>
+         * </summary>
+         * After the tag.
+         */
+//@@@@@   ^Before the tag.
+//@@      ^After the tag.
+        private void OutsideTheTag()
+        {
+        }
+
+        /**
+         * <summary>
+         * </summary>
+         * Between tags.
+         * <param name="x">
+         * </param>
+         */
+//@@@@    ^Between tags.
+        private void BetweenTags(int x)
+        {
+        }
+
     }
 }
