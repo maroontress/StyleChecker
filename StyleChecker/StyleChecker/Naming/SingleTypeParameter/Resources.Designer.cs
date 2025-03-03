80f5e6163ed073933dde61f2d9e51c1e14649b18
Tomohisa Tanaka <Tomohisa.Tanaka@gmail.com>
2025-03-03 13:15:01 +0900

    Fix SingleTypeParameter analyzer
    - Remove the period at the end of the sentence in the Code Fix titles

--- StyleChecker/StyleChecker/Naming/SingleTypeParameter/Resources.Designer.cs
+++ StyleChecker/StyleChecker/Naming/SingleTypeParameter/Resources.Designer.cs
@@ -10,7 +10,6 @@
 
 namespace StyleChecker.Naming.SingleTypeParameter {
     using System;
-    using System.Reflection;
     
     
     /// <summary>
@@ -20,7 +19,7 @@ namespace StyleChecker.Naming.SingleTypeParameter {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -40,7 +39,7 @@ namespace StyleChecker.Naming.SingleTypeParameter {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("StyleChecker.Naming.SingleTypeParameter.Resources", typeof(Resources).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("StyleChecker.Naming.SingleTypeParameter.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
